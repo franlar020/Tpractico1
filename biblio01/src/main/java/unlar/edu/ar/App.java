365d5f5f5d64a2ef5f913ffdc291e422789db56e
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-03-26 22:55:23 -0300

    Agregar archivos de configuración y código fuente para el proyecto biblio01

---
+++ biblio01/src/main/java/unlar/edu/ar/App.java
@@ -0,0 +1,17 @@
+package unlar.edu.ar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        System.out.println("probando xd ");
+
+        System.out.println("probando xd 2   ");
+    }
+}
